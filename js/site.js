e51fe7d018fce02d64dd9c7c27f54b16b180867e
Dakota Wall <dkwall17@gmail.com>
2022-03-20 20:09:54 +0300

    Coded the Javascript application
    Coded the JS application to perform the spec'd function to reverse a string using arrays.

--- js/site.js
+++ js/site.js
@@ -1,1 +1,31 @@
+// The Controller Function
+function getValue() {
+    // get user input of type string that will be reversed
 
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    // Invoke the Logic Function to reverse the string
+    let revString = reverseString(userString);
+
+    // call the View Function to update the UI
+    displayString(revString);
+}
+
+// The Logic Function(s)
+// Reverse the string
+function reverseString(userString) {
+    let revString = [];
+    for(let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    return revString;
+}
+
+// The View Function
+// Display the reversed string to the user
+function displayString(revString) {
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
+}
